1e71f7a8613e5c771499b926d4e36966614f2ee9
wata00913 <175d8639@gmail.com>
2022-12-16 13:25:46 +0900

    メモ一覧画面の簡易的なViewを作成

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/memo-list">Memo List</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/MemoListView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1>MemoList</h1>
+</template>
